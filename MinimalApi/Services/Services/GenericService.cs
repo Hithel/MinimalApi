3388294a7e94373d5019ace43ac65e50a2c16f46
Hithel <edward22cd@gmail.com>
2024-09-05 11:23:47 -0500

    feat: modification in generic services

--- MinimalApi/Services/Services/GenericService.cs
+++ MinimalApi/Services/Services/GenericService.cs
@@ -1,64 +1,89 @@
-﻿using MinimalApi.Repository.Interfaces;
+﻿using AutoMapper;
+using FluentValidation;
+using MinimalApi.Repository.Interfaces;
+using MinimalApi.Repository.UnitOfWork;
 using MinimalApi.Services.IServices;
 using System.Linq.Expressions;
 
-namespace MinimalApi.Services.Services;
-
-public abstract class GenericService<Vm, Dto, T> : IGenericService<Vm, Dto> where Vm : class where Dto : class where T : class
+namespace MinimalApi.Services.Services
 {
-    protected readonly IGenericRepository<T> _repository;
-    
-
-    protected GenericService(IGenericRepository<T> repository)
+    public abstract class GenericService<Vm, Dto, T> : IGenericService<Vm, Dto>
+        where Vm : class
+        where Dto : class
+        where T : class
     {
-        _repository = repository;
-    }
+        protected readonly IUnitOfWork _unitOfWork; // Unidad de Trabajo
+        private readonly IMapper _mapper;
+        private readonly IValidator<Dto> _validator;
 
-    public virtual async Task<Vm?> GetByIdAsync(int id)
-    {
-        var entity = await _repository.GetByIdAsync(id);
-
-    }
-
-    public virtual async Task<IEnumerable<Vm>> GetAllAsync()
-    {
-        var entities = await _repository.GetAllAsync();
-
-    }
-
-    public virtual async Task<IEnumerable<Vm>> FindByCondition(Expression<Func<Dto, bool>> predicate)
-    {
-        var entities = await _repository.FindByConditionAsync(e => predicate.Invoke(ConvertToDto(e)));
-
-    }
-
-    public virtual async Task<Vm> AddAsync(Dto dto)
-    {
-        var entity = ConvertToEntity(dto);
-        await _repository.AddAsync(entity);
-
-    }
-
-    public virtual async Task<Vm> UpdateAsync(Dto dto, int id)
-    {
-        var entity = await _repository.GetByIdAsync(id);
-        if (entity == null)
+        protected GenericService(IUnitOfWork unitOfWork, IMapper mapper, IValidator<Dto> validator)
         {
-            throw new KeyNotFoundException("Entity not found");
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _validator = validator;
         }
 
-        UpdateEntityFromDto(entity, dto);
-        await _repository.UpdateAsync(entity);
-    }
+        // Método abstracto que será implementado por cada servicio concreto para obtener el repositorio correcto
+        protected abstract IGenericRepository<T> GetRepository();
 
-    public virtual async Task DeleteAsync(int id)
-    {
-        var entity = await _repository.GetByIdAsync(id);
-        if (entity == null)
+        public virtual async Task<Vm?> GetByIdAsync(int id)
         {
-            throw new KeyNotFoundException("Entity not found");
+            var entity = await GetRepository().GetByIdAsync(id);
+            return _mapper.Map<Vm>(entity);
         }
 
-        await _repository.RemoveAsync(entity);
+        public virtual async Task<IEnumerable<Vm>> GetAllAsync()
+        {
+            var entities = await GetRepository().GetAllAsync();
+            return _mapper.Map<IEnumerable<Vm>>(entities);
+        }
+
+
+        public virtual async Task<Vm> AddAsync(Dto dto)
+        {
+            var validationResult = await _validator.ValidateAsync(dto);
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult.Errors);
+            }
+
+            var entity = _mapper.Map<T>(dto);
+            await GetRepository().AddAsync(entity);
+            await _unitOfWork.SaveAsync(); // Guardar cambios con UnitOfWork
+
+            return _mapper.Map<Vm>(entity);
+        }
+
+        public virtual async Task<Vm> UpdateAsync(Dto dto, int id)
+        {
+            var entity = await GetRepository().GetByIdAsync(id);
+            if (entity == null)
+            {
+                throw new KeyNotFoundException("Entity not found");
+            }
+
+            _mapper.Map(dto, entity);
+            await GetRepository().UpdateAsync(entity);
+            await _unitOfWork.SaveAsync(); // Guardar cambios con UnitOfWork
+
+            return _mapper.Map<Vm>(entity);
+        }
+
+        public virtual async Task DeleteAsync(int id)
+        {
+            var entity = await GetRepository().GetByIdAsync(id);
+            if (entity == null)
+            {
+                throw new KeyNotFoundException("Entity not found");
+            }
+
+            await GetRepository().RemoveAsync(entity);
+            await _unitOfWork.SaveAsync(); // Guardar cambios con UnitOfWork
+        }
+
+        private Dto ConvertToDto(T entity)
+        {
+            return _mapper.Map<Dto>(entity);
+        }
     }
 }
--- MinimalApi/Services/Services/UserService.cs
+++ MinimalApi/Services/Services/UserService.cs
@@ -1,7 +1,24 @@
-﻿using MinimalApi.Services.IServices;
+﻿using AutoMapper;
+using FluentValidation;
+using MinimalApi.Dtos.User;
+using MinimalApi.Models;
+using MinimalApi.Repository.Interfaces;
+using MinimalApi.Repository.UnitOfWork;
+using MinimalApi.Services.IServices;
+using MinimalApi.ViewModels.User;
 
 namespace MinimalApi.Services.Services;
 
-public class UserService
+public class UserService : GenericService<UserVm, UserDto, User>
 {
+    public UserService(IUnitOfWork unitOfWork, IMapper mapper, IValidator<UserDto> validator)
+        : base(unitOfWork, mapper, validator)
+    {
+
+    }
+
+    protected override IGenericRepository<User> GetRepository()
+    {
+        return _unitOfWork.Users; // Obteniendo el repositorio de Usuarios desde UnitOfWork
+    }
 }
